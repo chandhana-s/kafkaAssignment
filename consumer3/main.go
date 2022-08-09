0d96febeaabf879e20c1b46c98b925209cab22ae
Chandhana S <chandhana.s@MacBook-Pro.local>
2022-08-09 09:49:57 +0530

    add files kafka

---
+++ consumer3/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+func main() {
+
+	topic := "example-topic-123"
+
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		GroupID: "consumer-group-2",
+		Topic:   topic,
+	})
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			break
+		}
+		fmt.Printf("Message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+	}
+
+	if err := r.Close(); err != nil {
+		log.Fatal("Failed to close reader:", err)
+	}
+}
